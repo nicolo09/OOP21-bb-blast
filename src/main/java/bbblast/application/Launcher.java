623cc1349e4a6d6f7460e15d08347c169244f19d
Nicolò Guerra <nicolo.guerra@studio.unibo.it>
2022-03-24 10:05:35 +0100

    Adjusted Launcher class

--- src/main/java/bbblast/application/Launcher.java
+++ src/main/java/bbblast/application/Launcher.java
@@ -1,10 +1,22 @@
 package bbblast.application;
 
-public class Launcher {
+/**
+ * 
+ * Starting class for application.
+ *
+ */
+public final class Launcher {
 
+    private Launcher() {
+    }
+
+    /**
+     * Main method launched on application start.
+     * @param args
+     */
     public static void main(final String[] args) {
         // TODO Auto-generated method stub
-        
+
     }
 
 }
