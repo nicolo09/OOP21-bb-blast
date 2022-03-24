8e24735d5dbdd7b7880459b325755d922e6bb14c
Filippo Casadei <filippo.casadei9@studio.unibo.it>
2022-03-24 18:47:16 +0100

    Added Javadoc to Bubble Interface

--- src/main/java/bbblast/model/Bubble.java
+++ src/main/java/bbblast/model/Bubble.java
@@ -1,9 +1,20 @@
 package bbblast.model;
 
+/**
+ * 
+ * The Interface which models a Bubble.
+ *
+ */
 public interface Bubble {
 	
+	/**
+	 * @return the position of the Bubble
+	 */
 	Position getCoords();
 	
+	/**
+	 * @return the color of the Bubble
+	 */
 	COLOR getColor();
 
 }
