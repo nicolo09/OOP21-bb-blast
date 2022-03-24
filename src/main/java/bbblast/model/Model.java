76461e25a8f3c7988eeccd5f4ed47995a7d9697d
Filippo Casadei <filippo.casadei9@studio.unibo.it>
2022-03-24 16:13:48 +0100

    Added Javadoc to Model Interface

--- src/main/java/bbblast/model/Model.java
+++ src/main/java/bbblast/model/Model.java
@@ -6,24 +6,56 @@ import bbblast.utils.Settings;
 
 public interface Model {
 	
+	/**
+	 * updates the game values. 
+	 */
 	void update();
-	
+
+	/**
+	 * @return the collection of all Bubbles
+	 */
 	Collection<Bubble> getBubbles();
 	
+	/**
+	 * moves the cannon.
+	 */
 	void moveCannon();
-	
+
+	/**
+	 * makes the cannon shoot.
+	 */
 	void shootCannon();
-	
+
+	/**
+	 * @return the angle of the cannon
+	 */
 	int getCannonAngle();
-	
+
+	/**
+	 * @return the score of the current game
+	 */
 	int getScore();
-	
+
+	/**
+	 * @return true if the BubblesGrid reaches the bottom, false otherwise
+	 */
 	boolean isGameOver();
 	
+	/**
+	 * switches the Bubble inside the cannon.
+	 */
 	void switchBubble();
-	
+
+	/**
+	 * reads from a file the Settings to load.
+	 * @return the Settings to apply to the game
+	 */
 	Settings loadSettings();
-	
+
+	/**
+	 * writes on a file the current Setting.
+	 * @param s the current Settings
+	 */
 	void writeSettings(Settings s);
 	
 }
