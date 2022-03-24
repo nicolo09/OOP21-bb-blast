60d8018feb76ef3d419fe4f11c632a8d87809ba3
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2022-03-24 10:11:33 +0100

    Created interface Controller

---
+++ src/main/java/bbblast/controller/Controller.java
@@ -0,0 +1,50 @@
+package bbblast.controller;
+
+import java.util.Collection;
+
+/***
+ * The Controller
+ */
+public interface Controller {
+
+    /**
+     * @param v The view associated with the controller.
+     */
+    void setView(View v);
+
+    /**
+     * @param m The model associated with the controller.
+     */
+    void setModel(Model m);
+
+    /**
+     * @return the settings associated with the controller.
+     */
+    Settings loadSettings();
+
+    /**
+     * @param s the setting written on file.
+     */
+    void writeSettings(Settings s);
+
+    /**
+     * Starts the SinglePlayer Game.
+     */
+    void startSinglePlayerGame();
+
+    /**
+     * @return the collection of bubbles in the game.
+     */
+    Collection<Bubble> getBubbles();
+
+    /**
+     * moves the cannon.
+     */
+    void moveCannon();
+
+    /**
+     * shoots the cannon.
+     */
+    void shootCannon();
+
+}
