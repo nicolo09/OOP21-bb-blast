ec0292488ad1fa2368ed5e5d6fb8d0c2ed2696a9
Filippo Casadei <filippo.casadei9@studio.unibo.it>
2022-03-27 17:58:23 +0200

    Created Score Interface

---
+++ src/main/java/bbblast/utils/Score.java
@@ -0,0 +1,26 @@
+package bbblast.utils;
+
+import java.time.LocalDate;
+/**
+ * 
+ * The interface which models a Score
+ *
+ */
+public interface Score {
+	
+	/** 
+	 * @return the name of the player who got this Score
+	 */
+	String getName();
+	
+	/**
+	 * @return the Score's numeric value
+	 */
+	int getScoreValue();
+	
+	/**
+	 * @return the date when the player got this Score 
+	 */
+	LocalDate getDate();
+
+}
