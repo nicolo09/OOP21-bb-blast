f75eeb8e7b7ada13b47b0dbbff0807ff606217f6
Nicolò Guerra <guerragianni0503@gmail.com>
2022-03-27 16:39:52 +0200

    Created MainMenuView implementation

---
+++ src/main/java/bbblast/view/menu/MainMenuViewImpl.java
@@ -0,0 +1,87 @@
+package bbblast.view.menu;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+public class MainMenuViewImpl implements MainMenuView{
+
+    private static final double TITLEFONTSIZE = 22;
+    private static final double BUTTONS_MAX_WIDTH = 400;
+    private MainMenuViewController controller;
+    final private Scene scene;
+
+    private static final double MINWIDTH = 500;
+    private static final double MINHEIGHT = 300;
+    private static final double SPACING = 10;
+    
+    public MainMenuViewImpl() {
+        final VBox buttonsVBox = new VBox();
+        final BorderPane root = new BorderPane(buttonsVBox);
+        buttonsVBox.setFillWidth(true);
+        buttonsVBox.setSpacing(SPACING);
+
+        final Label lbl = new Label();
+        lbl.setText("Main Menu");
+        final Font menuFont = Font.font("Comic Sans MS", TITLEFONTSIZE);
+        lbl.setFont(menuFont);
+        lbl.setTextFill(Color.GOLD);
+
+        // Singleplayer button
+        final Button btnSinglePlayer = new Button("Singleplayer");
+        btnSinglePlayer.setOnMouseClicked(e -> {
+            this.controller.startSingleplayer();
+        });
+
+        // Multiplayer button
+        final Button btnMultiPlayer = new Button("Multiplayer");
+        btnMultiPlayer.setOnMouseClicked(e -> {
+            this.controller.startMultiplayer();
+        });
+
+        // Options button
+        final Button btnOptions = new Button("Options");
+        btnOptions.setOnMouseClicked(e -> {
+            this.controller.startOptionsMenu();
+        });
+
+        // Exit button
+        final Button btnExit = new Button("Exit");
+        btnExit.setOnMouseClicked(e -> {
+            this.controller.quit();
+        });
+        
+        root.setPadding(new Insets(0, 50, 10, 50));
+
+        btnSinglePlayer.setMaxWidth(BUTTONS_MAX_WIDTH);
+        btnMultiPlayer.setMaxWidth(BUTTONS_MAX_WIDTH);
+        btnOptions.setMaxWidth(BUTTONS_MAX_WIDTH);
+        btnExit.setMaxWidth(BUTTONS_MAX_WIDTH);
+        
+        buttonsVBox.setAlignment(Pos.CENTER);
+        buttonsVBox.getChildren().addAll(lbl, btnSinglePlayer, btnMultiPlayer, btnOptions, btnExit);
+        
+        this.scene = new Scene(root,MINWIDTH,MINHEIGHT);
+    }
+    
+    
+    @Override
+    public void setController(final MainMenuViewController controller) {
+        this.controller = controller;
+    }
+    
+    public Scene getScene() {
+        return this.scene;
+    }
+
+    @Override
+    public void show() {
+    }
+
+}
