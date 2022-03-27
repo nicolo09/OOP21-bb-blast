b36e517be35054d32a319d085d9627d5208f2e40
Nicolò Guerra <guerragianni0503@gmail.com>
2022-03-27 16:43:37 +0200

    Added getScene() method to return scene from MainMenu Views

--- src/main/java/bbblast/view/menu/MainMenuView.java
+++ src/main/java/bbblast/view/menu/MainMenuView.java
@@ -1,8 +1,12 @@
 package bbblast.view.menu;
 
+import javafx.scene.Scene;
+
 public interface MainMenuView {
     
     void setController(MainMenuViewController controller);
+    
+    Scene getScene();
     
     void show();
 
