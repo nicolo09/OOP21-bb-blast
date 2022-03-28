78e46a03f8b830e875139415d3d3d09096273bf3
Nicolò Guerra <nicolo.guerra@studio.unibo.it>
2022-03-28 14:12:18 +0200

    Added OptionViewController implementation

---
+++ src/main/java/bbblast/view/OptionViewControllerImpl.java
@@ -0,0 +1,37 @@
+package bbblast.view;
+
+import bbblast.view.options.OptionViewController;
+
+public class OptionViewControllerImpl implements OptionViewController {
+
+    @Override
+    public int getMasterVolume() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getMusicVolume() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getEffectsVolume() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void saveSettings() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void exitDiscarding() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
