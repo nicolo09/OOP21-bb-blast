6a066ce39981b88626a68064849246c8c80c260a
Nicolò Guerra <nicolo.guerra@studio.unibo.it>
2022-03-28 14:11:07 +0200

    Created Options view interface and implementation

---
+++ src/main/java/bbblast/view/options/OptionView.java
@@ -0,0 +1,11 @@
+package bbblast.view.options;
+
+import javafx.scene.Scene;
+
+public interface OptionView {
+
+    void setController(OptionViewController controller);
+    
+    Scene getScene();
+    
+}
