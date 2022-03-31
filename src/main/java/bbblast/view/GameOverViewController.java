73cd2e9d2cdeacb3088fcc4ab92a7346bdb0436d
Nicolò Guerra <nicolo.guerra@studio.unibo.it>
2022-03-31 11:58:43 +0200

    Added GameOverViewController

---
+++ src/main/java/bbblast/view/GameOverViewController.java
@@ -0,0 +1,9 @@
+package bbblast.view;
+
+public interface GameOverViewController {
+
+    String getScore();
+
+    void saveScore(String name);
+
+}
---
+++ src/main/java/bbblast/view/GameOverViewControllerImpl.java
@@ -0,0 +1,17 @@
+package bbblast.view;
+
+public class GameOverViewControllerImpl implements GameOverViewController {
+
+    @Override
+    public String getScore() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void saveScore(final String name) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
