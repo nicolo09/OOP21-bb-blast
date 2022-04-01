fc4e9a2ebeb292b677f07a4ff44c17762e345d51
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2022-04-01 16:28:46 +0200

    Created the implementation of Bubble in BubbleImpl

---
+++ src/main/java/bbblast/model/BubbleImpl.java
@@ -0,0 +1,47 @@
+package bbblast.model;
+
+/**
+ * The class that implements the bubble interface, used to rappresent a 2D
+ * bubble.
+ */
+public class BubbleImpl implements Bubble {
+
+    private final Position pos;
+    private final COLOR color;
+
+    /**
+     * @param b a bubble object. This constructor creates a new bubble with the same
+     *          characteristics as b.
+     */
+    public BubbleImpl(final BubbleImpl b) {
+        this(b.getCoords(), b.getColor());
+    }
+
+    /**
+     * @param p is the position of the new bubble
+     * @param c is the COLOR of the new bubble This constructor creates a new bubble
+     *          with the characteristics provided.
+     */
+    public BubbleImpl(final Position p, final COLOR c) {
+        this.pos = new Position(p.getX(), p.getY());
+        this.color = c;
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public COLOR getColor() {
+        return this.color;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Position getCoords() {
+        return new Position(this.pos.getX(), this.pos.getY());
+    }
+
+}
