b504981f0b583d66b4349ecdea4e699b451a2c51
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2022-04-02 16:54:42 +0200

    Added method getSameColorNeighbors, changed addBubble to recive a Bubble instead of a Position, minor formatting changes

--- src/main/java/bbblast/model/BubblesGrid.java
+++ src/main/java/bbblast/model/BubblesGrid.java
@@ -8,27 +8,35 @@ import java.util.Collection;
  *
  */
 public interface BubblesGrid {
-	
-	/**
-	 * @return the collection of Bubbles inside the grid
-	 */
-	Collection<Bubble> getBubbles();
-	
-	/**
-	 * @return the Y coordinate of the lowest row
-	 */
-	int getLastRowY();
-	
-	/**
-	 * adds the Bubble in the specified position.
-	 * @param p the position of the new Bubble 
-	 */
-	void addBubble(Position p);
-	
-	/**
-	 * removes the Bubble in the specified position.
-	 * @param p the position of the Bubble to remove
-	 */
-	void removeBubble(Position p);
+
+    /**
+     * @return the collection of Bubbles inside the grid
+     */
+    Collection<Bubble> getBubbles();
+
+    /**
+     * @return the Y coordinate of the lowest row
+     */
+    int getLastRowY();
+
+    /**
+     * adds the Bubble in the specified position.
+     * 
+     * @param b the new Bubble
+     */
+    void addBubble(Bubble b);
+
+    /**
+     * removes the Bubble in the specified position.
+     * 
+     * @param p the position of the Bubble to remove
+     */
+    void removeBubble(Position p);
+
+    /**
+     * @param b the Bubble to search its neighbors.
+     * @return the collection of same color neighbors as b.
+     */
+    Collection<Bubble> getSameColorNeighbors(Bubble b);
 
 }
