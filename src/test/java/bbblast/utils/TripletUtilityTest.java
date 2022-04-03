8d24a269d8aa07b4bfaa46cc9a9d260821057d76
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2022-04-03 17:21:03 +0200

    Created TripletUtilityTest

---
+++ src/test/java/bbblast/utils/TripletUtilityTest.java
@@ -0,0 +1,26 @@
+package bbblast.utils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class TripletUtilityTest {
+
+    @Test
+    public void testTripletSum() {
+        final Triplet<Integer, Integer, Integer> t1 = new TripletImpl<>(1, 2, 3);
+        final Triplet<Integer, Integer, Integer> t2 = new TripletImpl<>(-1, -2, -3);
+        final Triplet<Integer, Integer, Integer> t3 = new TripletImpl<>(3, 2, 0);
+        final Triplet<Integer, Integer, Integer> t4 = new TripletImpl<>(-3, 0, -2);
+
+        final Triplet<Integer, Integer, Integer> t5 = TripletUtility.add(t1, t2);
+        final Triplet<Integer, Integer, Integer> t6 = TripletUtility.add(t1, t3);
+        final Triplet<Integer, Integer, Integer> t7 = TripletUtility.add(t2, t4);
+        final Triplet<Integer, Integer, Integer> t8 = TripletUtility.add(t2, t3);
+        assertEquals(t5, new TripletImpl<>(0, 0, 0));
+        assertEquals(t6, new TripletImpl<>(4, 4, 3));
+        assertEquals(t7, new TripletImpl<>(-4, -2, -5));
+        assertEquals(t8, new TripletImpl<>(2, 0, -3));
+
+    }
+}
