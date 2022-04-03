837d71e65c44c42104b72948e8bb06ee236b41ea
Lorenzo Tagliani <lorenzo.tagliani2@studio.unibo.it>
2022-04-03 16:36:56 +0200

    Created AssetsLoader interface

---
+++ src/main/java/bbblast/view/singleplayer/AssetsLoader.java
@@ -0,0 +1,12 @@
+package bbblast.view.singleplayer;
+
+/**
+ * Interface model the assets loader.
+ */
+public interface AssetsLoader {
+    /**
+     * load the assets.
+     */
+    void load();
+
+}
