606edb3fb2681328cb06964cca863147004218d9
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2022-04-09 15:39:26 +0200

    Added import Position

--- src/main/java/bbblast/model/Bubble.java
+++ src/main/java/bbblast/model/Bubble.java
@@ -1,20 +1,22 @@
 package bbblast.model;
 
+import bbblast.utils.Position;
+
 /**
  * 
  * The Interface which models a Bubble.
  *
  */
 public interface Bubble {
-	
-	/**
-	 * @return the position of the Bubble
-	 */
-	Position getCoords();
-	
-	/**
-	 * @return the color of the Bubble
-	 */
-	COLOR getColor();
+
+    /**
+     * @return the position of the Bubble
+     */
+    Position getCoords();
+
+    /**
+     * @return the color of the Bubble
+     */
+    COLOR getColor();
 
 }
