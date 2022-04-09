5a13dbc2b7e554479646dfdfad5538bce140c5cf
Filippo Casadei <filippo.casadei9@studio.unibo.it>
2022-04-09 11:42:37 +0200

    Created PositionImpl class

---
+++ src/main/java/bbblast/utils/PositionImpl.java
@@ -0,0 +1,35 @@
+package bbblast.utils;
+
+public class PositionImpl implements Position {
+
+	@Override
+	public double getX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void translate(double dx, double dy) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setCoords(double x, double y) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Position getCopy() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
