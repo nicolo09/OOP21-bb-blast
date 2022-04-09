d22a961354b10fb31b7b922cf79522b217d6fa76
Filippo Casadei <filippo.casadei9@studio.unibo.it>
2022-04-09 11:53:19 +0200

    Implemented PositionImpl methods

--- src/main/java/bbblast/utils/PositionImpl.java
+++ src/main/java/bbblast/utils/PositionImpl.java
@@ -1,35 +1,65 @@
 package bbblast.utils;
 
+/**
+ * 
+ * Position Implementation.
+ *
+ */
 public class PositionImpl implements Position {
-
+	
+	private double x;
+	private double y;
+	
+	/**
+	 * Returns a Position pointing to specified location.
+	 * @param x the x coordinate
+	 * @param y the y coordinate
+	 */
+	public PositionImpl(final double x, final double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public double getX() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.x;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public double getY() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.y;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
-	public void translate(double dx, double dy) {
-		// TODO Auto-generated method stub
-
+	public void translate(final double dx, final double dy) {
+		this.x += dx;
+		this.y += dy;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
-	public void setCoords(double x, double y) {
-		// TODO Auto-generated method stub
-
+	public void setCoords(final double x, final double y) {
+		this.x = x;
+		this.y = y;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public Position getCopy() {
-		// TODO Auto-generated method stub
-		return null;
+		return new PositionImpl(this.x, this.y);
 	}
 
 }
