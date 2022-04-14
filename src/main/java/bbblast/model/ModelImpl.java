032e258d7017f51f1ddece767663a3c9226f8876
Nicolò Guerra <nicolo.guerra@studio.unibo.it>
2022-04-14 11:37:05 +0200

    Created ModelImpl

---
+++ src/main/java/bbblast/model/ModelImpl.java
@@ -0,0 +1,90 @@
+package bbblast.model;
+
+import java.util.Collection;
+import java.util.Map;
+
+import bbblast.controller.Controller;
+import bbblast.utils.Score;
+import bbblast.utils.Settings;
+
+public class ModelImpl implements Model {
+
+    @Override
+    public void update() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public Collection<Bubble> getBubbles() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void moveCannon(int angle) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void shootCannon() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public int getCannonAngle() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public Map<Integer, Integer> getScores() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean isGameOver() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void switchBubble() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public Settings loadSettings() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void writeSettings(Settings s) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public Collection<Score> loadScores() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void writeScore(Score s) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void setController(Controller controller) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
