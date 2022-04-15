a01fcd2f29c44e35bb887f3e0bba5adb6cd28137
Nicolò Guerra <nicolo.guerra@studio.unibo.it>
2022-04-15 11:59:51 +0200

    Added javadoc to GridInfo

--- src/main/java/bbblast/model/ModelImpl.java
+++ src/main/java/bbblast/model/ModelImpl.java
@@ -9,10 +9,17 @@ import bbblast.utils.Settings;
 
 public class ModelImpl implements Model {
 
+    private final GridInfo gridInfo;
+    private Controller controller;
+    
+    public ModelImpl(final GridInfo grid) {
+        this.gridInfo = grid;
+    }
+    
+    //TODO: Casa fa la gestione del movimento e dei rimbalzi
     @Override
     public void update() {
         // TODO Auto-generated method stub
-
     }
 
     @Override
@@ -21,24 +28,26 @@ public class ModelImpl implements Model {
         return null;
     }
 
+    //TODO: Emma fa il cannone
     @Override
-    public void moveCannon(int angle) {
+    public void moveCannon(final int angle) {
         // TODO Auto-generated method stub
-
     }
 
+    //TODO: Emma fa il cannone
     @Override
     public void shootCannon() {
         // TODO Auto-generated method stub
-
     }
 
+    //TODO: Emma fa il cannone
     @Override
     public int getCannonAngle() {
         // TODO Auto-generated method stub
         return 0;
     }
 
+    //TODO: Taglia fa gli score
     @Override
     public Map<Integer, Integer> getScores() {
         // TODO Auto-generated method stub
@@ -51,6 +60,7 @@ public class ModelImpl implements Model {
         return false;
     }
 
+    //TODO: Emma fa il cannone
     @Override
     public void switchBubble() {
         // TODO Auto-generated method stub
@@ -69,22 +79,23 @@ public class ModelImpl implements Model {
 
     }
 
+    //TODO: Taglia fa gli scores
     @Override
     public Collection<Score> loadScores() {
         // TODO Auto-generated method stub
         return null;
     }
 
+    //TODO: Taglia fa gli scores
     @Override
-    public void writeScore(Score s) {
+    public void writeScore(final Score s) {
         // TODO Auto-generated method stub
 
     }
 
     @Override
-    public void setController(Controller controller) {
-        // TODO Auto-generated method stub
-
+    public void setController(final Controller controller) {
+        this.controller = controller;
     }
 
 }
