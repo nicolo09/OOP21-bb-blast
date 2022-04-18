a6f31a16d9409e14417c5129528432c93ed38615
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2022-04-18 19:47:14 +0200

    Created tests for Cannon methods

---
+++ src/test/java/bbblast/model/CannonTest.java
@@ -0,0 +1,60 @@
+package bbblast.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.Test;
+
+import bbblast.utils.PositionImpl;
+
+public class CannonTest {
+
+    @Test
+    public void testCannonPersistence() {
+        final Cannon c = new CannonImpl(new PositionImpl(0, 0), 60, 60, null);
+        assertFalse(c.toString().equals(""), "Cannon has a toString");
+    }
+
+    @Test
+    public void testCannonMove() {
+        final Cannon c = new CannonImpl(new PositionImpl(0, 0), 60, 60, null);
+        final int start = c.getAngle();
+        final int change = 45;
+        c.move(change);
+        final int end = c.getAngle();
+        assertEquals(start, CannonImpl.START_ANGLE, "Cannon starts at starting value degrees");
+        assertNotEquals(start, end, "Moving the cannon changes the angle");
+        assertEquals(change, c.getAngle(), "The cannon angle is set correctly");
+        c.move(CannonImpl.START_ANGLE);
+        c.move(-change);
+        assertEquals(c.getAngle(), CannonImpl.START_ANGLE, "The angle is negative, so it wasn't changed");
+        c.move(CannonImpl.START_ANGLE);
+        c.move(CannonImpl.MIN_ANGLE - 1);
+        assertEquals(c.getAngle(), CannonImpl.START_ANGLE, "The angle is outside of the range, so it wasn't changed");
+        c.move(CannonImpl.START_ANGLE);
+        c.move(CannonImpl.MAX_ANGLE + 1);
+        assertEquals(c.getAngle(), CannonImpl.START_ANGLE, "The angle is outside of the range, so it wasn't changed");
+    }
+
+    @Test
+    public void testGetCurrentlyLoadedBubble() {
+        final Cannon c = new CannonImpl(new PositionImpl(0, 0), 60, 60, null);
+        final var b1 = c.getCurrentlyLoadedBubble();
+        final var b2 = c.shoot();
+        assertEquals(b1, b2, "The bubble currently loaded is the one shot");
+
+    }
+
+    @Test
+    public void testCannonShoot() {
+        final Cannon c = new CannonImpl(new PositionImpl(0, 0), 60, 60, null);
+        assertNotEquals(c.shoot(), null, "A bubble isn't null");
+        c.move(CannonImpl.MAX_ANGLE);
+        assertNotEquals(c.shoot(), null, "A bubble isn't null");
+        c.move(CannonImpl.MAX_ANGLE);
+        assertNotEquals(c.shoot(), null, "A bubble isn't null");
+
+    }
+
+}
