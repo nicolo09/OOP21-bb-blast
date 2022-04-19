52c1fed40d50cf3747f6c23fe745c55e5825e36e
Lorenzo Tagliani <lorenzo.tagliani2@studio.unibo.it>
2022-04-19 16:12:14 +0200

    Positive result from score tests

--- src/test/java/bbblast/utils/ScoreTest.java
+++ src/test/java/bbblast/utils/ScoreTest.java
@@ -1,46 +1,28 @@
 package bbblast.utils;
 
-import java.time.LocalDate;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-/**
- * 
- * Score implementation
- *
- */
+import org.junit.jupiter.api.Test;
+
 public class ScoreTest {
     
-    private String name = "";
-    private int score = 0;
-    private LocalDate date = null;
+    final private ScoreImpl s1 = new ScoreImpl("lorenzo", 1000);
+    final private ScoreImpl s2 = new ScoreImpl("emma", 8);
     
-    /**
-     * Return the score of the actual player
-     * @param name for the player's name
-     * @param score for the player's score
-     * @return 
-     */
-    public void ScoreImpl(final String name, final int score) {
-        this.name = name;
-        this.score = score;
-        this.date = LocalDate.now();
-    }
-    /**
-     * {@inheritDoc}
-     */
-    public String getName() {
-        return this.name;
-    }
-    /**
-     * {@inheritDoc}
-     */
-    public int getScoreValue() {
-        return this.score;
-    }
-    /**
-     * {@inheritDoc}
-     */
-    public LocalDate getDate() {
-        return this.date;
+    @Test
+    public void testscore() {
+        //test 1
+        assertTrue(s1.getName().equals("lorenzo"),"The two names are equal");
+        assertTrue(s1.getScoreValue()==(1000),"The two score are equal");
+        assertNotEquals(s1.getDate(), null, "The date exist");
+        
+        //test 2
+        assertFalse(s2.equals(s1), "They are different");
+        
+        //test 3
+        assertTrue(s2.equals(s2), "They are the same");
     }
     
 }
