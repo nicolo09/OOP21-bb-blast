ca23974b9d7c751d8e6e553682ecd0755de33653
Nicolò Guerra <nicolo.guerra@studio.unibo.it>
2022-04-19 14:23:06 +0200

    Implemented tests for FilePersister

---
+++ src/test/java/bbblast/utils/persister/PersonForTest.java
@@ -0,0 +1,56 @@
+package bbblast.utils.persister;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class PersonForTest implements Serializable{
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 5552387880392078106L;
+    private final String name;
+    private final String surname;
+    private final int age;
+
+    public PersonForTest() {
+        this.name = null;
+        this.surname = null;
+        this.age = 0;
+    }
+
+    public PersonForTest(final String name, final String surname, final int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(age, name, surname);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getSurname() {
+        return this.surname;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        final PersonForTest other = (PersonForTest) obj;
+        return this.age == other.age && Objects.equals(this.name, other.name)
+                && Objects.equals(this.surname, other.surname);
+    }
+}
