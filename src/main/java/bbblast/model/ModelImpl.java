6118001f0d0395aeaa1f545f4be3eb5388dd9960
Nicolò Guerra <nicolo.guerra@studio.unibo.it>
2022-04-21 15:58:09 +0200

    Implemented model's methods

--- src/main/java/bbblast/model/ModelImpl.java
+++ src/main/java/bbblast/model/ModelImpl.java
@@ -1,25 +1,37 @@
 package bbblast.model;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Optional;
 
 import bbblast.controller.GameOver;
 import bbblast.controller.GameOverImpl;
 import bbblast.controller.Controller;
+import bbblast.utils.Position;
+import bbblast.utils.PositionImpl;
 import bbblast.utils.Score;
-import bbblast.utils.Settings;
 
 public class ModelImpl implements Model {
 
+    private static final double CANNONVERTICALOFFSETPERCENT = 0.9;
+    // Set a speed
+    private static final int BUBBLESPEED = 0;
     private final GridInfo gridInfo;
     private Controller controller;
-    
+    private final BubblesGrid grid;
+    private final Cannon cannon;
+
     public ModelImpl(final GridInfo grid) {
         this.gridInfo = grid;
+        this.grid = new BubblesGridImpl(grid);
+        final Position bubbleSpawnPosition = new PositionImpl(gridInfo.getPointsWidth() / 2,
+                gridInfo.getPointsHeight() * CANNONVERTICALOFFSETPERCENT);
+        this.cannon = new CannonImpl(bubbleSpawnPosition, this.controller.getFPS(), BUBBLESPEED,
+                new BubbleGeneratorImpl(COLOR.allExceptGrey()));
     }
-    
-    //TODO: Casa fa la gestione del movimento e dei rimbalzi
+
+    // TODO: Casa fa la gestione del movimento e dei rimbalzi
     @Override
     public void update() {
         // TODO Auto-generated method stub
@@ -27,30 +39,28 @@ public class ModelImpl implements Model {
 
     @Override
     public Collection<Bubble> getBubbles() {
-        // TODO Auto-generated method stub
-        return null;
+        final var result = new HashSet<>(grid.getBubbles());
+        result.add(cannon.getCurrentlyLoadedBubble());
+        // TODO: Add moving bubble to result
+        return result;
     }
 
-    //TODO: Emma fa il cannone
     @Override
     public void moveCannon(final int angle) {
-        // TODO Auto-generated method stub
+        cannon.move(angle);
     }
 
-    //TODO: Emma fa il cannone
     @Override
     public void shootCannon() {
-        // TODO Auto-generated method stub
+        cannon.shoot();
     }
 
-    //TODO: Emma fa il cannone
     @Override
     public int getCannonAngle() {
-        // TODO Auto-generated method stub
-        return 0;
+        return cannon.getAngle();
     }
 
-    //TODO: Taglia fa gli score
+    // TODO: Taglia fa gli score
     @Override
     public Map<Integer, Integer> getScores() {
         // TODO Auto-generated method stub
@@ -59,32 +69,28 @@ public class ModelImpl implements Model {
 
     @Override
     public Optional<GameOver> gameOver() {
-        //TODO: End of bubbleGrid reached
-        if (false) {
+        if (this.grid.endReached()) {
             return Optional.of(new GameOverImpl(this.getScores()));
         }
         return Optional.empty();
     }
 
-    //TODO: Emma fa il cannone
     @Override
     public void switchBubble() {
-        // TODO Auto-generated method stub
-
+        cannon.exchange();
     }
 
-    //TODO: Taglia fa gli scores
+    // TODO: Taglia fa gli scores
     @Override
     public Collection<Score> loadScores() {
         // TODO Auto-generated method stub
         return null;
     }
 
-    //TODO: Taglia fa gli scores
+    // TODO: Taglia fa gli scores
     @Override
     public void writeScore(final Score s) {
         // TODO Auto-generated method stub
-
     }
 
     @Override
