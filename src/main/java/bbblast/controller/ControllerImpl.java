6d35889f6a67b9d8b5a1f6dd8465152826deb434
Nicolò Guerra <nicolo.guerra@studio.unibo.it>
2022-04-21 16:33:16 +0200

    Code cleaning

--- src/main/java/bbblast/controller/ControllerImpl.java
+++ src/main/java/bbblast/controller/ControllerImpl.java
@@ -16,9 +16,11 @@ import bbblast.utils.Settings;
 import bbblast.utils.persister.FilePersister;
 import bbblast.utils.persister.Persister;
 import bbblast.view.View;
-
+/**
+ * Implementation of {@link Controller}.
+ */
 public class ControllerImpl implements Controller {
-    
+
     //TODO Move this to a constants class
     private static final String SEPARATOR = FileSystems.getDefault().getSeparator();
     private static final Path SETTINGSPATH = Path.of(System.getProperty("user.home") + SEPARATOR + ".bbblast" + SEPARATOR + "settings");
@@ -26,22 +28,34 @@ public class ControllerImpl implements Controller {
     private Model mainModel;
     private GameLoop loop;
     private final Persister<Settings> settingsPersister = new FilePersister<>(SETTINGSPATH, Settings.class);
-    
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void setView(final View v) {
         this.mainView = v;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void setModel(final Model m) {
         this.mainModel = m;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Optional<Settings> loadSettings() {
         return settingsPersister.load();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean writeSettings(final Settings s) {
         try {
@@ -53,12 +67,15 @@ public class ControllerImpl implements Controller {
         return false;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void startSinglePlayerGame() {
         //View setup
         mainView.startSinglePlayerGame();
         //Model setup
-        
+
         //Gameover Handler
         final Updatable gameOverHandler = new GameOverHandlerPolling(this.mainModel, this.mainView);
         //GameLoop setup
@@ -69,38 +86,58 @@ public class ControllerImpl implements Controller {
         loop.startLoop();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void pauseGame() {
         loop.pauseLoop();
     }
-    
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Collection<Bubble> getBubbles() {
         return mainModel.getBubbles();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void moveCannon(final int angle) {
         mainModel.moveCannon(angle);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void shootCannon() {
         mainModel.shootCannon();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int getScore() {
         // TODO Auto-generated method stub
         return 0;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void saveScore(final Score score) {
         // TODO Auto-generated method stub
-        
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int getFPS() {
         return this.loop.getFPS();
