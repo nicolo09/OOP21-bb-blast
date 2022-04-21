89e965531525ae2bb12543649db62aa420324922
Filippo Casadei <filippo.casadei9@studio.unibo.it>
2022-04-21 11:28:35 +0200

    Fixed minor checkstyle warnings in BubblesDrawerImpl

--- src/main/java/bbblast/view/singleplayer/BubblesDrawerImpl.java
+++ src/main/java/bbblast/view/singleplayer/BubblesDrawerImpl.java
@@ -10,51 +10,57 @@ import bbblast.view.singleplayer.assetsloader.ImageAssetsLoader;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
-
+/**
+ * 
+ * Implementation of BubbleDrawer.
+ *
+ */
 public class BubblesDrawerImpl implements BubblesDrawer {
 
 	private final GridInfo infos;
 	private final Canvas canvas;
 	private final GraphicsContext gc;
-	private final AssetsLoader<Image> loader; 
-	
+	private final AssetsLoader<Image> loader;
+
 	/**
 	 * Creates a new BubblesDrawer.
+	 * 
 	 * @param parent the canvas where the BubblesDrawer will draw
-	 * @param infos the informations needed to draw the Bubbles consistently
+	 * @param infos  the informations needed to draw the Bubbles consistently
 	 */
-	public BubblesDrawerImpl(final Canvas parent, GridInfo infos) {
+	public BubblesDrawerImpl(final Canvas parent, final GridInfo infos) {
 		this.canvas = parent;
 		this.infos = infos;
 		this.gc = parent.getGraphicsContext2D();
 		this.loader = new CachingAssetsLoader<>(new ImageAssetsLoader());
 	}
-	
+
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
-	public void drawBubbles(List<Bubble> l) {
+	public void drawBubbles(final List<Bubble> l) {
 		gc.clearRect(0, 0, canvas.getWidth(), canvas.getLayoutY());
-		for (var b : l) {
-			gc.drawImage(getImg(b), adjustPos(b.getCoords().getX())*getRateoOnX(), adjustPos(b.getCoords().getY())*getRateoOnY(), getRateoOnX(), getRateoOnY());
+		for (final var b : l) {
+			gc.drawImage(getImg(b), adjustPos(b.getCoords().getX()) * getRateoOnX(),
+					adjustPos(b.getCoords().getY()) * getRateoOnY(), getRateoOnX(), getRateoOnY());
 		}
 	}
-	
-	private Image getImg(Bubble b) {
+
+	private Image getImg(final Bubble b) {
 		return loader.load(b.getColor().toString() + "Bubble.png");
 	}
-	
-	private double adjustPos(double coord) {
+
+	private double adjustPos(final double coord) {
 		return coord - infos.getBubbleRadius();
 	}
-	
+
 	private double getRateoOnX() {
-		return canvas.getWidth()/infos.getBubbleWidth();
+		return canvas.getWidth() / infos.getBubbleWidth();
 	}
-	
+
 	private double getRateoOnY() {
-		return canvas.getHeight()/infos.getBubbleHeight();
+		return canvas.getHeight() / infos.getBubbleHeight();
 	}
 
 }
