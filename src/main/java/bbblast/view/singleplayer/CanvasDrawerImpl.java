400068629a85da78cebf630b09e57c63ddb8c486
Filippo Casadei <filippo.casadei9@studio.unibo.it>
2022-04-21 11:44:33 +0200

    Created CanvasDrawerImpl class

---
+++ src/main/java/bbblast/view/singleplayer/CanvasDrawerImpl.java
@@ -0,0 +1,23 @@
+package bbblast.view.singleplayer;
+
+import java.util.Collection;
+
+import bbblast.model.Bubble;
+import bbblast.model.Cannon;
+/**
+ * 
+ * Implementation of CanvasDrawer.
+ *
+ */
+public class CanvasDrawerImpl implements CanvasDrawer {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void drawOnCanvas(Collection<Bubble> bubbles, Cannon cannon) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
