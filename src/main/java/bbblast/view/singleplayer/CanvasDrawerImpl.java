443c879624b8427d6857a1ea96dada4a2285af60
Filippo Casadei <filippo.casadei9@studio.unibo.it>
2022-04-21 12:08:11 +0200

    Implemented CanvasDrawerImpl methods

--- src/main/java/bbblast/view/singleplayer/CanvasDrawerImpl.java
+++ src/main/java/bbblast/view/singleplayer/CanvasDrawerImpl.java
@@ -4,6 +4,9 @@ import java.util.Collection;
 
 import bbblast.model.Bubble;
 import bbblast.model.Cannon;
+import bbblast.model.GridInfo;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 /**
  * 
  * Implementation of CanvasDrawer.
@@ -11,13 +14,32 @@ import bbblast.model.Cannon;
  */
 public class CanvasDrawerImpl implements CanvasDrawer {
 
+	private final BubblesDrawer bubbleD;
+	private final CannonDrawer cannonD;
+	private final Canvas canvas;
+	private final GraphicsContext gc;
+	
+	/**
+	 * Creates a new CanvasDrawerImpl.
+	 * @param canvas where the elements will be drawn
+	 * @param infos the informations to draw the elements on the canvas
+	 */
+	public CanvasDrawerImpl(final Canvas canvas, final GridInfo infos) {
+		this.bubbleD = new BubblesDrawerImpl(canvas, infos);
+		this.cannonD = new CannonDrawerImpl(); // TO BE DEFINED
+		this.canvas = canvas;
+		this.gc = canvas.getGraphicsContext2D();
+	}
+	
+	
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
 	public void drawOnCanvas(Collection<Bubble> bubbles, Cannon cannon) {
-		// TODO Auto-generated method stub
-
+		gc.clearRect(0, 0, canvas.getWidth(), canvas.getLayoutY());
+		bubbleD.drawBubbles(bubbles);
+		cannonD.drawCannon(cannon);
 	}
 
 }
