3d401def2bce8abaf1e6e30f18bd706e59c49669
Lorenzo Tagliani <lorenzo.tagliani2@studio.unibo.it>
2022-04-21 21:24:00 +0200

    Created Game View implementation

---
+++ src/main/java/bbblast/view/singleplayer/GameViewImpl.java
@@ -0,0 +1,35 @@
+package bbblast.view.singleplayer;
+
+public class GameViewImpl implements GameView{
+    
+    @Override
+    public void playMusic() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void pauseMusic() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void playSoundEffect(Sound e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void show() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void update() {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
