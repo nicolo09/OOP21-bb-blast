a88329f55072d902a145259b67a5174c075eb1f5
Nicolò Guerra <nicolo.guerra@studio.unibo.it>
2022-04-23 10:10:52 +0200

    Added Javadoc to MainMenuViewController and implementation

--- src/main/java/bbblast/view/menu/MainMenuViewControllerImpl.java
+++ src/main/java/bbblast/view/menu/MainMenuViewControllerImpl.java
@@ -2,11 +2,13 @@ package bbblast.view.menu;
 
 import bbblast.view.View;
 import javafx.application.Platform;
-
+/**
+ * Implements a {@link MainMenuViewController} referring a main {@link View}.
+ */
 public class MainMenuViewControllerImpl implements MainMenuViewController {
 
     private final View mainView;
-    
+
     /**
      * 
      * @param mainView the main view on which method will be called
@@ -15,21 +17,33 @@ public class MainMenuViewControllerImpl implements MainMenuViewController {
         this.mainView = mainView;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void startSingleplayer() {
         mainView.startSinglePlayerGame();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void startMultiplayer() {
         mainView.startMultiplayerGame();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void startOptionsMenu() {
         mainView.startOptionsMenu();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void quit() {
         Platform.exit();
