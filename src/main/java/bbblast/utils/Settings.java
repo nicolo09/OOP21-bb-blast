9c7c7a22d4044c0e51ff1a4bc85817b2ae05c983
Nicolò Guerra <nicolo.guerra@studio.unibo.it>
2022-04-23 10:05:40 +0200

    Added Javadoc to Settings and SettingsImpl

--- src/main/java/bbblast/utils/Settings.java
+++ src/main/java/bbblast/utils/Settings.java
@@ -1,11 +1,24 @@
 package bbblast.utils;
-
+/**
+ * Class that models game settings.
+ */
 public interface Settings {
-
+    /**
+     * 
+     * @return the master volume
+     */
     int getMasterVolume();
 
+    /**
+     * 
+     * @return the music volume
+     */
     int getMusicVolume();
 
+    /**
+     * 
+     * @return the effects volume
+     */
     int getEffectsVolume();
 
 }
--- src/main/java/bbblast/utils/SettingsImpl.java
+++ src/main/java/bbblast/utils/SettingsImpl.java
@@ -3,6 +3,9 @@ package bbblast.utils;
 import java.io.Serializable;
 import java.util.Objects;
 
+/**
+ * Implementation of {@link Settings} that can be serialized ({@link Serializable})
+ */
 public class SettingsImpl implements Settings, Serializable{
 
     /**
@@ -12,54 +15,74 @@ public class SettingsImpl implements Settings, Serializable{
     private final int masterVolume;
     private final int musicVolume;
     private final int effectsVolume;
-    
+    /**
+     * 
+     * @param masterVolume
+     * @param musicVolume
+     * @param effectsVolume
+     */
     public SettingsImpl(final int masterVolume, final int musicVolume, final int effectsVolume){
         this.masterVolume = masterVolume;
         this.musicVolume = musicVolume;
         this.effectsVolume = effectsVolume;
     }
-    
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int getMasterVolume() {
         return this.masterVolume;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int getMusicVolume() {
         return this.musicVolume;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int getEffectsVolume() {
         return this.effectsVolume;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int hashCode() {
         return Objects.hash(effectsVolume, masterVolume, musicVolume);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean equals(final Object obj) {
         if (this == obj) {
-            return true;            
+            return true;
         }
         if (obj == null) {
-            return false;            
+            return false;
         }
         if (getClass() != obj.getClass()) {
-            return false;            
+            return false;
         }
         final SettingsImpl other = (SettingsImpl) obj;
         return this.effectsVolume == other.effectsVolume && this.masterVolume == other.masterVolume
                 && this.musicVolume == other.musicVolume;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String toString() {
         return "SettingsImpl [masterVolume=" + this.masterVolume + ", musicVolume=" + this.musicVolume
                 + ", effectsVolume=" + this.effectsVolume + "]";
     }
-
-    
 }
