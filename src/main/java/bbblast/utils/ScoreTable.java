bc481f0f54f0a667bed7bcd77a6d1ddaa57a104d
Nicolò Guerra <nicolo.guerra@studio.unibo.it>
2022-04-24 12:00:24 +0200

    Code cleaning

--- src/main/java/bbblast/utils/ScoreTable.java
+++ src/main/java/bbblast/utils/ScoreTable.java
@@ -9,29 +9,33 @@ import java.util.List;
  * Class that memorize score in a list
  *
  */
-public class ScoreTable implements Serializable{
+public class ScoreTable implements Serializable {
 
     /**
      * Generated serial ID
      */
     private static final long serialVersionUID = 2214274898079476338L;
     private final List<Score> l;
-    
+
     /**
      * Create an empty list
      */
     public ScoreTable() {
         this.l = new ArrayList<Score>();
     }
+
     /**
      * Add a score to the list
+     * 
      * @param s
      */
     public void addScore(final Score s) {
         l.add(s);
     }
+
     /**
      * Return the list
+     * 
      * @return
      */
     public List<Score> getList() {
