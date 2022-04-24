04bc5d6d85cab4654e7be9603380c235141868f4
Lorenzo Tagliani <lorenzo.tagliani2@studio.unibo.it>
2022-04-24 17:10:29 +0200

    Updated contructor method

--- src/main/java/bbblast/view/singleplayer/GameViewImpl.java
+++ src/main/java/bbblast/view/singleplayer/GameViewImpl.java
@@ -1,96 +1,79 @@
 package bbblast.view.singleplayer;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Path;
-
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.DataLine.Info;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.SourceDataLine;
-import javax.sound.sampled.UnsupportedAudioFileException;
-
-import static javax.sound.sampled.AudioSystem.getAudioInputStream;
-import static javax.sound.sampled.AudioFormat.Encoding.PCM_SIGNED;
-
+import javafx.beans.value.ChangeListener;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
-public abstract class GameViewImpl implements GameView{
-    
-    private static final String SEPARATOR = FileSystems.getDefault().getSeparator();
-    private static final Path PATH = Path
-            .of(System.getProperty("user.home") + SEPARATOR + ".bbblast-test" + SEPARATOR + "music.mp3");
-    
+public class GameViewImpl implements GameView {
+
     private SingleplayerGameViewController controller;
-    final AudioFilePlayer player = new AudioFilePlayer();
-    
+    private final BackgroundImage b = new BackgroundImage(new Image("background.jpg", 500, 300, false, true),
+            BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+    private final Background bGround = new Background(b);
+
+    private static final double MINWIDTH = 500;
+    private static final double MINHEIGHT = 300;
+
+    final private Scene scene;
+
     public GameViewImpl() {
+        final VBox bg = new VBox();
+        final BorderPane root = new BorderPane(bg);
+        bg.setFillWidth(true);
+        root.setBackground(bGround);
+
+        //serve col canvas
+        //final ChangeListener<Number> resizeListener = (observable, oldValue, newValue) ->{
+        //    canvas.setHeight(this.scene.getHeight());
+        //    canvas.setWidth(this.scene.getWidth());
+        //};  
+        //this.scene.widthProperty().addListener(resizeListener);
+        //this.scene.heightProperty().addListener(resizeListener);
         
+        this.scene = new Scene(root, MINWIDTH, MINHEIGHT);
     }
 
     @Override
     public void playMusic() {
         // TODO Auto-generated method stub
-        public void play(String filePath) {
-            final File file = new File(filePath);
-     
-            try (final AudioInputStream in = getAudioInputStream(file)) {
-                 
-                final AudioFormat outFormat = getOutFormat(in.getFormat());
-                final Info info = new Info(SourceDataLine.class, outFormat);
-     
-                try (final SourceDataLine line =
-                         (SourceDataLine) AudioSystem.getLine(info)) {
-     
-                    if (line != null) {
-                        line.open(outFormat);
-                        line.start();
-                        stream(getAudioInputStream(outFormat, in), line);
-                        line.drain();
-                        line.stop();
-                    }
-                }
-     
-            } catch (UnsupportedAudioFileException 
-                   | LineUnavailableException 
-                   | IOException e) {
-                throw new IllegalStateException(e);
-            }
-        }
-        
+
     }
 
     @Override
     public void pauseMusic() {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
-    public void playSoundEffect(Sound e) {
+    public void playSoundEffect() {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
-    public void setController(SingleplayerGameViewController controller) {
+    public void setController(final SingleplayerGameViewController controller) {
         this.controller = controller;
-        // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void update() {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public Scene getScene() {
         // TODO Auto-generated method stub
-        return null;
+        return this.scene;
     }
-    
+
 }
