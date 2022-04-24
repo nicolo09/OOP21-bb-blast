188ca3468db34ee83bd655752a7bf81e8146cad9
Lorenzo Tagliani <lorenzo.tagliani2@studio.unibo.it>
2022-04-24 17:11:37 +0200

    Created SingleplayerGameViewController implementation

---
+++ src/main/java/bbblast/view/singleplayer/SingleplayerGameViewControllerImpl.java
@@ -0,0 +1,39 @@
+package bbblast.view.singleplayer;
+
+import java.util.Collection;
+
+import bbblast.model.Bubble;
+
+public class SingleplayerGameViewControllerImpl implements SingleplayerGameViewController {
+
+    @Override
+    public Collection<Bubble> getBubbles() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void cannonShoot(int a) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public int getScore() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void pause() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void exit() {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
