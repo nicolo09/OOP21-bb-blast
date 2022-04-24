8638b5f5ab8fced647e8d9c666d63b9fae4b4ff8
Nicolò Guerra <nicolo.guerra@studio.unibo.it>
2022-04-24 12:01:06 +0200

    Added Javadoc to tests classes

--- src/test/java/bbblast/model/ColorTest.java
+++ src/test/java/bbblast/model/ColorTest.java
@@ -4,11 +4,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.Test;
-
+/**
+ * Test class for {@link COLOR} enum.
+ */
 public class ColorTest {
+
+    /**
+     * Tests the method which returns all colors but grey.
+     */
     @Test
     public void allExceptGrey() {
         assertEquals(COLOR.values().length - 1, COLOR.allExceptGrey().size(), "All except gray method returned a collections with wrong length");
-        assertFalse(COLOR.allExceptGrey().contains(COLOR.GREY),"All except gray method returned a collections that contains grey");
+        assertFalse(COLOR.allExceptGrey().contains(COLOR.GREY), "All except gray method returned a collections that contains grey");
     }
 }
--- src/test/java/bbblast/utils/persister/PersonForTest.java
+++ src/test/java/bbblast/utils/persister/PersonForTest.java
@@ -3,6 +3,10 @@ package bbblast.utils.persister;
 import java.io.Serializable;
 import java.util.Objects;
 
+/**
+ * Generic person class to test persistance layer independently from other
+ * components.
+ */
 public class PersonForTest implements Serializable {
     /**
      * 
@@ -12,35 +16,62 @@ public class PersonForTest implements Serializable {
     private final String surname;
     private final int age;
 
+    /**
+     * 
+     */
     public PersonForTest() {
         this.name = null;
         this.surname = null;
         this.age = 0;
     }
 
+    /**
+     * 
+     * @param name
+     * @param surname
+     * @param age
+     */
     public PersonForTest(final String name, final String surname, final int age) {
         this.name = name;
         this.surname = surname;
         this.age = age;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int hashCode() {
         return Objects.hash(age, name, surname);
     }
 
+    /**
+     * 
+     * @return this person name
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     * 
+     * @return this person surname
+     */
     public String getSurname() {
         return this.surname;
     }
 
+    /**
+     * 
+     * @return this person age
+     */
     public int getAge() {
         return this.age;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean equals(final Object obj) {
         if (this == obj) {
