559ffb4a3d630f31e8bd9c5ccf1e12790c428feb
Nicolò Guerra <nicolo.guerra@studio.unibo.it>
2022-04-25 12:11:03 +0200

    Removed useless imports

--- src/main/java/bbblast/model/Model.java
+++ src/main/java/bbblast/model/Model.java
@@ -4,10 +4,8 @@ import java.util.Collection;
 import java.util.Map;
 import java.util.Optional;
 
-import bbblast.controller.Controller;
 import bbblast.controller.gameloop.Updatable;
 import bbblast.controller.gameover.GameOver;
-import bbblast.utils.Score;
 
 /**
  * 
