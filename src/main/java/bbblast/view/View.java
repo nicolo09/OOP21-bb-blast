beb998da13700c5161ab84272d398c17b724cd7e
Nicolò Guerra <nicolo.guerra@studio.unibo.it>
2022-04-25 12:11:14 +0200

    Code cleaning

--- src/main/java/bbblast/view/View.java
+++ src/main/java/bbblast/view/View.java
@@ -10,39 +10,49 @@ import bbblast.controller.gameover.GameOver;
  *
  */
 public interface View extends Updatable {
+
     /**
      * Set the controller associated with this view.
+     * 
      * @param controller
      */
     void setController(Controller controller);
+
     /**
      * Goes to the main menu.
      */
     void goToMainMenu();
+
     /**
      * Starts a singleplayer game.
      */
     void startSinglePlayerGame();
+
     /**
      * Starts a multiplayer game.
      */
     void startMultiplayerGame();
+
     /**
      * Shows an option menu
      */
     void startOptionsMenu();
+
     /**
      * Shows a game over screen.
      */
     void gameOver(GameOver gameOverEvent);
+
     /**
      * Shows this view.
      */
     void show();
+
     /**
      * Displays an error
+     * 
      * @param error the error to display
      */
-    void showError(String error); 
+    void showError(String error);
 
 }
