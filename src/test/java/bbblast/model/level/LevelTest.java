e48cd1f1da25270233c5436798d5f177b6ae8dd1
Filippo Casadei <filippo.casadei9@studio.unibo.it>
2022-04-25 12:10:04 +0200

    Minor modifies in LevelTest class

--- src/test/java/bbblast/model/level/LevelTest.java
+++ src/test/java/bbblast/model/level/LevelTest.java
@@ -25,6 +25,8 @@ import bbblast.utils.persister.Persister;
  *
  */
 public class LevelTest {
+	private static final int FPS = 60;
+	private static final int SPEED = 5;
 	private static final String SEPARATOR = FileSystems.getDefault().getSeparator();
 	private static final Path PATH = Path
 			.of(System.getProperty("user.home") + SEPARATOR + ".bbblast-test" + SEPARATOR + "levelTest.json");
@@ -36,7 +38,7 @@ public class LevelTest {
 	public void testLevelCreation() {
 		final GridInfo infos = new RegularHexGridInfo(10, 10, 1);
 		final BubbleGenerator generator = new BubbleGeneratorImpl(List.of(COLOR.values()));
-		final Level lvl = new LevelImpl(infos, generator);
+		final Level lvl = new LevelImpl(infos, generator, FPS, SPEED);
 
 		lvl.fillGameBubblesGrid(1);
 		assertEquals(infos.getBubbleWidth(), lvl.getGameBubblesGrid().getBubbles().size(),
@@ -54,7 +56,7 @@ public class LevelTest {
 	public void testLevelSavingAndLoading() {
 		final GridInfo saveInfos = new RegularHexGridInfo(20, 50, 50);
 		final BubbleGenerator saveGenerator = new BubbleGeneratorImpl(List.of(COLOR.BLUE, COLOR.RED, COLOR.GREEN));
-		final Level saveLevel = new LevelImpl(saveInfos, saveGenerator);
+		final Level saveLevel = new LevelImpl(saveInfos, saveGenerator, FPS, SPEED);
 		final Persister<Level> persister = new FilePersister<>(PATH, Level.class);
 
 		try {
@@ -69,7 +71,7 @@ public class LevelTest {
 	}
 
 	/**
-	 * Testing LevelImpl's test method.
+	 * Testing LevelImpl's equals method.
 	 */
 	@Test
 	public void testLevelEquals() {
@@ -77,17 +79,16 @@ public class LevelTest {
 		final GridInfo differentInfos = new RegularHexGridInfo(10, 10, 1);
 		final BubbleGenerator originGenerator = new BubbleGeneratorImpl(List.of(COLOR.BLUE, COLOR.RED, COLOR.GREEN));
 		final BubbleGenerator differentGenerator = new BubbleGeneratorImpl(List.of(COLOR.values()));
-		final Level originLvl = new LevelImpl(originInfos, originGenerator);
-		final Level differentLvl1 = new LevelImpl(differentInfos, originGenerator);
-		final Level differentLvl2 = new LevelImpl(originInfos, differentGenerator);
-		final Level differentLvl3 = new LevelImpl(originInfos, originGenerator);
+		final Level originLvl = new LevelImpl(originInfos, originGenerator, FPS, SPEED);
+		final Level differentLvl1 = new LevelImpl(differentInfos, originGenerator, FPS, SPEED);
+		final Level differentLvl2 = new LevelImpl(originInfos, differentGenerator, FPS, SPEED);
+		final Level differentLvl3 = new LevelImpl(originInfos, originGenerator, FPS, SPEED);
 
 		assertNotEquals(originLvl, differentLvl1, "Two levels with different grid infos are different");
 		assertNotEquals(originLvl, differentLvl2, "Two levels with different generators are different");
-		assertEquals(originLvl, differentLvl3,
-				"Two levels created with same grid infos and generators are initially the same");
 		differentLvl3.fillGameBubblesGrid(1); // Filling the first row
 		assertNotEquals(originLvl, differentLvl3, "Two levels initially equals are different when filling a row");
+		//NOTE: when generating two levels with same parameters they could still be different, because the Cannon is generated randomly  
 	}
 
 }
