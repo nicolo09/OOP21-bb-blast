e4fad954f9545e355584fd1c2f962e97ca2d68c2
Lorenzo Tagliani <lorenzo.tagliani2@studio.unibo.it>
2022-04-25 16:33:42 +0200

    Added and removed methods from SingleplayerGameViewController

--- src/main/java/bbblast/view/singleplayer/SingleplayerGameViewController.java
+++ src/main/java/bbblast/view/singleplayer/SingleplayerGameViewController.java
@@ -3,39 +3,32 @@ package bbblast.view.singleplayer;
 import java.util.Collection;
 
 import bbblast.model.Bubble;
-import bbblast.model.CannonImpl;
+import javafx.scene.input.KeyEvent;
 
 /**
  * The interface of the SinglePlayer Game Controller.
  */
 public interface SingleplayerGameViewController {
-
-    int a = CannonImpl.START_ANGLE;
     
-    
+    /**
+     * Take a key in input and choose the right action.
+     * @param e is the input key.
+     */
+    void inputCheck(KeyEvent e);
     /**
      * @return the Collection of bubbles.
      */
     Collection<Bubble> getBubbles();
-
-    /**
-     * @param a the Angle at which the cannon shoots at.
-     */
-    void cannonShoot();
-
+    
     /**
      * @return the score.
      */
     int getScore();
 
     /**
-     * pauses the game.
-     */
-    void pause();
-
-    /**
      * exits the game.
      */
     void exit();
+    
 
 }
