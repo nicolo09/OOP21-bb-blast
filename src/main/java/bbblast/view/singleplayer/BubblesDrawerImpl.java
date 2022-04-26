329bdeaa9b3a04977effa0292c5ffaec87647022
Nicolò Guerra <nicolo.guerra@studio.unibo.it>
2022-04-26 18:47:13 +0200

    Fixed bubble radius calculation in BubblesDrawerImpl

--- src/main/java/bbblast/view/singleplayer/BubblesDrawerImpl.java
+++ src/main/java/bbblast/view/singleplayer/BubblesDrawerImpl.java
@@ -45,7 +45,7 @@ public class BubblesDrawerImpl implements BubblesDrawer {
 			//posX and posY are the coordinates which represents the top-left "corner" of the Bubble to draw
 			final var posX = b.getCoords().getX() - infos.getBubbleRadius();
 			final var posY = b.getCoords().getY() - 2 * infos.getBubbleRadius() / Math.sqrt(3);
-			gc.drawImage(getImg(b), posX * getRateoOnX(), posY * getRateoOnY(), getRateoOnX(), getRateoOnY());
+			gc.drawImage(getImg(b), posX * getRateoOnX(), posY * getRateoOnY(), infos.getBubbleRadius() * getRateoOnX(), infos.getBubbleRadius() * getRateoOnY());
 		}
 	}
 
